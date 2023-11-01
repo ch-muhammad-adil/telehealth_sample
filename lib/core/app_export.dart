c0902ef7fa7d1368a3227e533d318339321476d1
ch-muhammad-adil <chmuhammadadil7@gmail.com>
2023-11-01 08:10:08 +0000

    Code Synced by DhiWise

---
+++ lib/core/app_export.dart
@@ -0,0 +1,12 @@
+export 'package:muhammad_s_telehealth_app/core/constants/constants.dart';
+export 'package:muhammad_s_telehealth_app/core/utils/image_constant.dart';
+export 'package:muhammad_s_telehealth_app/core/utils/navigator_service.dart';
+export 'package:muhammad_s_telehealth_app/core/utils/size_utils.dart';
+export 'package:muhammad_s_telehealth_app/localization/app_localization.dart';
+export 'package:muhammad_s_telehealth_app/routes/app_routes.dart';
+export 'package:muhammad_s_telehealth_app/theme/app_decoration.dart';
+export 'package:muhammad_s_telehealth_app/theme/custom_text_style.dart';
+export 'package:muhammad_s_telehealth_app/theme/theme_helper.dart';
+export 'package:muhammad_s_telehealth_app/widgets/custom_image_view.dart';
+export '../theme/custom_button_style.dart';
+export 'package:muhammad_s_telehealth_app/core/utils/date_time_utils.dart';
